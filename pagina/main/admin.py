5fd06a11df13c07f1f1a4cad62a3c8385972dc86
BautBodrone <watobodrone@gmail.com>
2021-04-26 17:59:33 -0300

    abm Insumos y abm Viajes
    En viaje falta la cascada de los insumos

--- pagina/main/admin.py
+++ pagina/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import combi,chofer,insumo, viaje
 
 # Register your models here.
+admin.site.register(combi)
+admin.site.register(chofer)
+admin.site.register(insumo)
+admin.site.register(viaje)
--- pagina/main/models.py
+++ pagina/main/models.py
@@ -10,7 +10,7 @@ class chofer(models.Model):
     informacion_contacto = models.CharField(max_length=200)
 
     def __str__(self):
-        return (self.nombre+self.apellido)
+        return (self.nombre+" "+self.apellido)
 
 
 class combi(models.Model):
@@ -20,3 +20,23 @@ class combi(models.Model):
 
     def __str__(self):
         return self.patente
+
+class insumo(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=100)
+    precio = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(999)])
+    cantidad = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(999)])
+
+    def __str__(self):
+        return self.nombre+" "+self.descripcion
+
+class viaje(models.Model):
+    descripcion = models.CharField(max_length=100)
+    fecha = models.DateTimeField()
+    lugar_salida = models.CharField(max_length=100)
+    lugar_llegada = models.CharField(max_length=100)
+    chofer =  models.ForeignKey(chofer, default=None, on_delete=models.CASCADE)
+    combi =  models.ForeignKey(combi, default=None, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.lugar_salida+" "+self.lugar_salida+" "+self.fecha)
