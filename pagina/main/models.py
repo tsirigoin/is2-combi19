fa2360fca12c53c8c1aa41d507550f2272a697c2
BautBodrone <watobodrone@gmail.com>
2021-05-06 10:19:49 -0300

    Agregado campos unicos dobles
    Cuando se quiere ingresar nueva ruta,lugar o viaje ahora chechea antes de agregar si los campos estan repetidos

--- pagina/main/models.py
+++ pagina/main/models.py
@@ -14,7 +14,7 @@ class chofer(models.Model):
 
 
 class combi(models.Model):
-    patente = models.CharField(max_length=20)
+    patente = models.CharField(max_length=20, unique=True)
     tipo = models.CharField(max_length=100)
     capacidad = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(999)])
 
@@ -22,7 +22,7 @@ class combi(models.Model):
         return self.patente
 
 class insumo(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100, unique=True)
     descripcion = models.CharField(max_length=100)
     precio = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(999)])
     cantidad = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(999)])
@@ -33,6 +33,9 @@ class insumo(models.Model):
 class ruta(models.Model):
     origen = models.CharField(max_length=100)
     destino = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = ('origen', 'destino',)
 
     def __str__(self):
         return self.origen+" "+self.destino
@@ -45,12 +48,18 @@ class viaje(models.Model):
     combi =  models.ForeignKey(combi, default=None, on_delete=models.CASCADE)
     insumo = models.ForeignKey(insumo, default=None, on_delete=models.CASCADE) #falta correguir
 
+    class Meta:
+        unique_together = ('chofer', 'fecha', 'ruta',)
+
     def __str__(self):
         return (self.descripcion+" "+self.lugar_salida+" "+self.lugar_llegada+" "+str(self.fecha))
 
 class lugar(models.Model):
     localidad = models.CharField(max_length=150)
     provincia = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = ('localidad', 'provincia',)
 
     def __str__(self):
         return (self.localidad+" "+self.provincia)
