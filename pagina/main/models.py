c5a506b3a720f8d157a717cfd6cca51978980064
BautBodrone <watobodrone@gmail.com>
2021-05-11 11:11:01 -0300

    Cambio varios
    Se implementaron varios cambio a la base de datos entre ellos:
    -se agrego el rprecio al viaje.
    - el viaje puede tener 0 o varios insumos.
    -los precio no pueden ser menot que 0.
     - la patente de la combi no puede tener menos de 6 caracteres.
    - la fecha del viaje no puede ser menor que mañana.
    -el insumo no puede tener una cantidad negativa

--- pagina/main/models.py
+++ pagina/main/models.py
@@ -1,21 +1,22 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator, int_list_validator,ValidationError
+import datetime
 from users.models import Chofer
 
 # Create your models here.
 
 class Combi(models.Model):
-    patente = models.CharField(max_length=20, unique=True)
-    tipo = models.CharField(max_length=100)
+    patente = models.CharField(max_length=10, unique=True, validators=[MinLengthValidator(6)])
+    tipo = models.CharField(choices={('chico','Chico'),('mediano', 'Mediano'),('grande','Grande')},max_length=10)
     capacidad = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(999)])
 
-    def __str__(self):
+    def __str__(self):  
         return self.patente
 
 class Insumo(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
     descripcion = models.CharField(max_length=100)
-    precio = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(999)])
+    precio = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     cantidad = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(999)])
 
     def __str__(self):
@@ -33,14 +34,17 @@ class Ruta(models.Model):
 
 class Viaje(models.Model):
     descripcion = models.CharField(max_length=100)
-    fecha = models.DateTimeField()
+    fecha = models.DateField(validators=[MinValueValidator(datetime.date.today()+datetime.timedelta(hours=24))])
+    hora = models.TimeField(default=None)
     ruta =  models.ForeignKey(Ruta, default=None, on_delete=models.CASCADE)
     chofer =  models.ForeignKey(Chofer, default=None, on_delete=models.CASCADE)
     combi =  models.ForeignKey(Combi, default=None, on_delete=models.CASCADE)
-    insumo = models.ForeignKey(Insumo, default=None, on_delete=models.CASCADE) #falta correguir
+    precio = models.DecimalField(default=None, max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    insumo = models.ManyToManyField(Insumo,blank=True)
 
     class Meta:
         unique_together = ('chofer', 'fecha', 'ruta',)
+
 
     def __str__(self):
         return (self.descripcion+" "+self.ruta.origen+" "+self.ruta.destino+" "+str(self.fecha))
