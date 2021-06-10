0b48e50ba991b90537e678c74bc5d524f2a8838a
Tomás Irigoin <49447588+tsirigoin@users.noreply.github.com>
2021-06-10 03:58:31 -0300

    Listados en Admin corregidos y arreglados.

--- pagina/main/admin.py
+++ pagina/main/admin.py
@@ -2,10 +2,35 @@ from django.contrib import admin
 from .models import Combi, Insumo, Viaje, Lugar, Ruta, Comentario, Pasajero
 
 # Register your models here.
-admin.site.register(Combi)
-admin.site.register(Insumo)
+@admin.register(Combi)
+class CombiAdmin(admin.ModelAdmin):
+	list_display = (
+		'patente',
+		'tipo',
+		'capacidad',
+	)
+@admin.register(Insumo)
+class InsumoAdmin(admin.ModelAdmin):
+	list_display = (
+		'nombre',
+		'descripcion',
+		'precio',
+		'cantidad',
+	)
 admin.site.register(Ruta)
 admin.site.register(Viaje)
 admin.site.register(Lugar)
-admin.site.register(Comentario)
-admin.site.register(Pasajero)
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+	list_display = (
+		'usuario',
+		'texto',
+		'viaje',
+	)
+@admin.register(Pasajero)
+class PasajeroAdmin(admin.ModelAdmin):
+	list_display = (
+		'usuario',
+		'estado',
+		'dni',
+	)
